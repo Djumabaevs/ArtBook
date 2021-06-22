c5cfe81390521c9a5ab6037776700ac8ffa5f6c8
BakytDzhumabev <djumabaevb@gmail.com>
2021-06-23 01:25:57 +0600

    Initial Commit

---
+++ ArtBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArtBook
+//
+//  Created by Bakyt Dzhumabaev on 23/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
